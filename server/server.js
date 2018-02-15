c1f9494a23f5b697ae36f4b716f8e850fa2208b0
andonguyen <do.nguyen@gmail.com>
2018-02-15 13:27:05 +1100

    first commit

---
+++ server/server.js
@@ -0,0 +1,33 @@
+require('appmetrics-dash').attach();
+require('appmetrics-prometheus').attach();
+
+const appName = require('./../package').name;
+const express = require('express');
+const log4js = require('log4js');
+const localConfig = require('./config/local.json');
+const path = require('path');
+
+const logger = log4js.getLogger(appName);
+const app = express();
+const serviceManager = require('./services/service-manager');
+require('./services/index')(app);
+require('./routers/index')(app);
+
+// Add your code here
+
+const port = process.env.PORT || localConfig.port;
+app.listen(port, function(){
+  logger.info(`msbhelloworld listening on http://localhost:${port}/appmetrics-dash`);
+  
+  logger.info(`msbhelloworld listening on http://localhost:${port}`);
+  
+  
+});
+
+app.use(function (req, res, next) {
+  res.sendFile(path.join(__dirname, '../public/assets', '404.html'));
+})
+
+app.use(function (err, req, res, next) {
+  res.sendFile(path.join(__dirname, '../public/assets', '500.html'));
+})
